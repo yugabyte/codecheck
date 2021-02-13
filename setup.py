efde0a8e1fbce364360627870b8e2d36b0fb4c77
Mikhail Bautin <mbautin@users.noreply.github.com>
2021-02-13 10:03:51 -0800

    Fix URL

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='codecheck',
     version='1.0.0',
-    url='git@github.com:mbautin/codecheck.git',
+    url='https://github.com/mbautin/codecheck',
     author='Mikhail Bautin',
     author_email='mbautin@users.noreply.github.com',
     description='A tool for quickly running checks on a set of source code files in a project',
